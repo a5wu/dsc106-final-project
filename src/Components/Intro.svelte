250a03522e8274e697421da6987e1390dd94bafa
lmartinez-c <130008961+lmartinez-c@users.noreply.github.com>
2024-06-09 04:58:04 -0700

    Update Intro.svelte

--- src/Components/Intro.svelte
+++ src/Components/Intro.svelte
@@ -3,9 +3,9 @@
 
 <section>
   <p class="body-text">
-    <a href="https://en.wikipedia.org/wiki/PageRank">PageRank</a> is an algorithm developed by Larry Page and Sergey Brin in 1996
+    <a href="https://en.wikipedia.org/wiki/PageRank" target="_blank">PageRank</a> is an algorithm developed by Larry Page and Sergey Brin in 1996
     while they were PHD students at Stanford University.  
-    <span class="bold">bold text</span> In this article we will reconstruct this famous algorithm 
+    <span class="bold"></span> In this article we will reconstruct this famous algorithm 
     in order to learn how it works.
   </p>
   <p class="body-text"></p>
