9710ae8029147977d7d92a8b4cd7e7dbdc4932da
lmartinez-c <130008961+lmartinez-c@users.noreply.github.com>
2024-06-09 05:06:30 -0700

    Update Title.svelte

--- src/Components/Title.svelte
+++ src/Components/Title.svelte
@@ -1,8 +1,8 @@
 <section id="intro">
   <h1 id="intro-hed">PageRank</h1>
-  <h1 class="intro-sub">Exploring the Alogorithm that powered Google 1.0</h1>
+  <h1 class="intro-sub">Exploring the Algorithm that powered Google 1.0</h1>
   <h3 id="intro__date">
-    <a href="https://twitter.com/" target="_blank">Aleck Wu</a>, May 2024
+    <a href="https://twitter.com/" target="_blank">Aleck Wu</a> & <a href="https://youtube.com/" target="_blank">Luis Martinez-Chun</a>, May 2024
   </h3>
 </section>
 
